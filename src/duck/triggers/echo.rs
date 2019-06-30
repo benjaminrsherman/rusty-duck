18668c2d0deaed41af1307b6d796a5e3bd56b898
Benjamin Sherman <benjamin@bensherman.io>
2019-06-30 18:03:09 -0400

    Fix bug with empty echo message

--- src/duck/triggers/echo.rs
+++ src/duck/triggers/echo.rs
@@ -7,9 +7,11 @@ use serenity::prelude::*;
 #[command]
 #[description = "Echoes the given message"]
 pub fn echo(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
-    let content = &args.rest();
+    let content = args.rest();
 
-    utils::delay_send(&ctx.http, &msg.channel_id, content, 1);
+    if content.len() > 0 {
+        utils::delay_send(&ctx.http, &msg.channel_id, content, 1);
+    }
 
     Ok(())
 }
